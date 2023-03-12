a3bd1554d9309d385e268ba9c19f1cda9ece7640
Shobhit <121398721+orpic@users.noreply.github.com>
2023-03-12 14:50:08 +0530

    add babel-loader configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+      },
     ],
   },
 };
